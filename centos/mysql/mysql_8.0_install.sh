d9fd74596a1bd96f6890c1d83e2afa17ab322db6
LX <lixs@billbear.cn>
2023-10-18 16:16:48 +0800

    [新增] MySQL8.0 安装脚本

--- centos/mysql/mysql_8.0_install.sh
+++ centos/mysql/mysql_8.0_install.sh
@@ -9,4 +9,5 @@ sudo yum install wget -y
 sudo rpm -Uvh https://dev.mysql.com/get/mysql80-community-release-el7-3.noarch.rpm
 
 # 安装
+echo -e "\033[1;33m Install MySQL 8.x \033[0m"
 sudo yum install mysql-server -y
